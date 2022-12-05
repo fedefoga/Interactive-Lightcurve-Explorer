7dd6674be2bd0885372f55a5715d242dc7004c04
Fede Fogantini <47119227+fedefoga@users.noreply.github.com>
2022-12-05 11:07:21 -0300

    Add files via upload

---
+++ hid.py
@@ -0,0 +1,338 @@
+import os, sys
+import numpy as np
+
+import matplotlib as mpl
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+
+from matplotlib.widgets import SpanSelector, Slider, Button
+
+from stingray.events import EventList
+from stingray.gti import create_gti_from_condition
+
+import seaborn as sn
+sn.set_theme(context='paper', style='darkgrid', palette='deep', font='sans', font_scale=1)
+
+def consecutive(data, stepsize=1):
+    return np.split(data, np.where(np.diff(data) != stepsize)[0]+1)
+
+def update_plot(pararameters):
+
+    tbin = parameters[0]
+    ecut = parameters[1]
+    tmin = parameters[2]
+    tmax = parameters[3]
+
+    soft_events = events.filter_energy_range([emin,ecut])
+    hard_events = events.filter_energy_range([ecut,emax])
+
+    hard_lc = hard_events.to_lc(tbin)
+    soft_lc = soft_events.to_lc(tbin)
+
+    local_time = 0.5*(hard_lc.time+soft_lc.time)/1000
+    local_time -= static_tmin
+
+    # Filters for time and high quality hardness
+    mask1 = (local_time>tmin) & (local_time<tmax)
+    mask2 = (hard_lc.counts>1) & (soft_lc.counts>1)
+    mask = mask1 & mask2
+    indx = np.where(mask)[0]
+    
+
+    time = local_time[indx]-min(local_time[indx])  
+    intensity = hard_lc.countrate[indx] + soft_lc.countrate[indx]
+    hardness = hard_lc.counts[indx] / soft_lc.counts[indx]
+
+    # Create colormap from time array
+    norm = mpl.colors.Normalize(vmin=min(time), vmax=max(time))
+    colors = norm(time)
+
+    # Plot lines
+    line1.set_data(time,intensity)
+    line2.set_data(hardness,intensity)
+
+    # Plot colored scatter data points
+    offsets = np.c_[time,intensity]
+    scat1.set_offsets(offsets)
+    scat1.set_array(colors)
+
+    offsets = np.c_[hardness,intensity]
+    scat2.set_offsets(offsets)
+    scat2.set_array(colors)
+
+    # Set axis limits and ticks
+    ax1.set_xlim(-0.1, 1.05*time.max())
+    ax1.set_ylim(0.975*intensity.min(), 1.025*intensity.max())
+    ax2.set_xlim(0.975*hardness.min(), 1.025*hardness.max())
+
+    ticks = np.geomspace(hardness.min(), hardness.max(), 5, endpoint=True)
+    ax2.set_xticks(ticks)
+    ax2.set_xticklabels([f'${x:.2f}$' for x in ticks])
+
+    ticks = np.linspace(0, time.max(), 5, endpoint=True)
+    ax1.set_xticks(ticks)
+    ax1.set_xticklabels([f'${x:.1f}$' for x in ticks])
+
+    # Update title with lightcurve data
+    ax1_title.set_text(f'Min:${min(intensity):.1f}$   Max:${max(intensity):.1f}$   \
+    Avg:${np.mean(intensity):.1f}\\pm{np.std(intensity):.1f}$')
+
+    #Update time axis with reference time
+    ax1_xlabel.set_text(f'Time (ks since {min(local_time[indx]):.1f}) ks')
+
+    fig.canvas.draw_idle()
+
+
+
+def read_span_selector(xmin,xmax):
+
+    kmin, kmax = np.searchsorted(static_time, (xmin, xmax))
+    kmax = min(len(static_time) - 1, kmax)
+
+    parameters[2] = static_time[kmin]
+    parameters[3] = static_time[kmax]
+
+    parameters[0] = timebins[sld_tbin.val]
+    parameters[1] = energies[sld_ecut.val]
+
+    tbin_lab.set_text(f'Bin time : ${parameters[0]:.1f}$ s')
+    ecut_lab.set_text(f'Cutoff energy : ${parameters[1]:.1f}$ keV')
+
+    update_plot(parameters)
+    fig.canvas.draw_idle()
+
+
+
+def read_sliders(val):
+
+    parameters[0] = timebins[sld_tbin.val]
+    parameters[1] = energies[sld_ecut.val]
+
+    tbin_lab.set_text(f'Bin time : ${parameters[0]:.1f}$ s')
+    ecut_lab.set_text(f'Cutoff energy : ${parameters[1]:.1f}$ keV')
+
+    update_plot(parameters) 
+    fig.canvas.draw_idle()   	
+    
+    
+
+def clear_axes(event):
+
+    timespan.set_visible(False)
+   
+    line1.set_data([],[])
+    line2.set_data([],[])
+
+    offsets = np.c_[[],[]]
+    scat1.set_offsets(offsets)
+    scat1.set_array([])
+    scat2.set_offsets(offsets)
+    scat2.set_array([])
+            
+    sld_tbin.val = nbins//2
+    sld_ecut.val = nbins//2
+    parameters[0] = timebins[sld_tbin.val]
+    parameters[1] = energies[sld_ecut.val]
+#    parameters = np.zeros(4)
+
+    tbin_lab.set_text(f'Bin time : ${parameters[0]:.1f}$ s')
+    ecut_lab.set_text(f'Cutoff energy : ${parameters[1]:.1f}$ keV')
+
+    ax1_title.set_text('')
+    ax1_xlabel.set_text('Time (ks)')
+
+    fig.canvas.draw_idle()   	
+
+
+    
+## ======================================================================================
+
+if __name__ == "__main__":
+
+    ## Read event data
+    ## -------------------
+
+    input_data = sys.argv[1:]
+    if len(input_data)==1: 
+        events = EventList.read(sys.argv[1], 'hea')
+    elif len(input_data)==2:
+        evA = EventList.read(sys.argv[1], 'hea')
+        evB = EventList.read(sys.argv[2], 'hea')
+        if evA.mission != evB.mission : 
+            print('Event lists from different missions!')
+            print('Cannot join. Exit.')
+            sys.exit()
+        events = evA.join(evB)
+    else:
+        print('Please enter at least 1 HEASoft event list. Exit.')
+        sys.exit()
+    
+    lc_raw = events.to_lc(100)
+    gti0 = create_gti_from_condition(lc_raw.time, lc_raw.countrate > 1)
+    events.gti = gti0
+
+    
+    ## Mission dependent parameters
+    ## --------------------------------
+
+    mission = events.mission
+    instrument = events.instr
+    tstart = events.mjdref + min(lc_raw.time)/86400
+
+
+    if mission.lower()=='nustar':
+        emin = 3
+        emax = 79
+        bint = 50   ## binning for static lightcurve
+        nbins = 15
+        energies = np.geomspace(4, 30, nbins, endpoint=True)
+        timebins = np.geomspace(1,200, nbins, endpoint=True)
+        idx = [100//bint, -100//bint]
+   
+    elif mission.lower()=='nicer':
+        emin = 0.4
+        emax = 12
+        bint = 15
+        nbins = 15
+        energies = np.geomspace(0.8,8, nbins, endpoint=True)
+        timebins = np.geomspace(1, 30, nbins, endpoint=True)
+        idx = [50//bint, -50//bint]
+
+    elif mission.lower()=='xmm':
+        emin = 0.5
+        emax = 12
+        bint = 50
+        nbins = 15
+        energies = np.geomspace(1,8, nbins, endpoint=True)
+        timebins = np.geomspace(1, 200, nbins, endpoint=True)
+        idx = [100//bint, -100//bint]
+
+    else:
+        print('Please enter XMM, NICER or NUSTAR event list. Exit.')
+        sys.exit()
+        
+
+    ## Design plot
+    ## -----------------------------------------------------
+
+    cm = plt.cm.get_cmap('viridis')
+    norm = mpl.colors.Normalize(vmin=0, vmax=1)
+    
+    fig = plt.figure(figsize=plt.figaspect(0.6), dpi=160, constrained_layout=True)
+    gs = gridspec.GridSpec(2,4, wspace=0.075, hspace=0.075, 
+            height_ratios=[0.6,1], width_ratios=[0.04,1,1,0.04])
+
+    ax0 = fig.add_subplot(gs[0, :])
+    ax1 = fig.add_subplot(gs[1, 1])
+    ax2 = fig.add_subplot(gs[1, 2], sharey=ax1)
+
+    ax0.tick_params(size=0, top=False, labelbottom=False, labeltop=False, 
+    labelsize='small')
+    ax1.tick_params(size=0, which='both', labelleft=False, labelright=False,
+     labelsize='small')
+    ax2.tick_params(size=0, which='both', labelleft=False, labelright=False,
+     labelsize='small')
+
+    ax0_top = ax0.secondary_xaxis('top')
+    ax0_top.tick_params(pad=1, direction='in', size=2, which='both', 
+    labelsize='small')
+    ax0_top.set_xlabel(f'Time (ks since ${tstart:.1f}$ MJD; ${bint:g}$s bin)',
+     fontsize='small')
+
+    ax0.annotate(f'{mission.upper():10s}\n${emin:g}-{emax:g}$ keV', xy=(1.02,0.5), 
+    xycoords='axes fraction', rotation=-90, fontsize='small', va='center', ha='center')
+    
+    ax0.set_ylabel('Count rate (cts/sec)', fontsize='small')
+    ax1_xlabel = ax1.set_xlabel('Time (ks)', fontsize='small')
+    ax2.set_xlabel('Hard/Soft', fontsize='small')
+    ax2.set_xscale('log')
+    ax2.minorticks_off()
+    ax2.axvline(1, lw=1.5, c='k', zorder=1)
+
+    # Title container for updating parameters
+    ax1_title = ax1.set_title('', fontsize='small', loc='center', pad=1)
+
+    ## Create static lightcurve
+    ## -------------------------
+
+    total_events = events.filter_energy_range([emin,emax])
+    total_lc = total_events.to_lc(bint)
+    gti = create_gti_from_condition(total_lc.time, total_lc.counts>1)
+    total_lc.gti = gti
+    total_lc.apply_gtis()
+
+    static_time = total_lc.time/1000
+    static_tmin = min(static_time)
+    static_time -= static_tmin
+
+    split_lc = total_lc.split(min_gap=bint, min_points=3)
+    tmin = min(split_lc[0].time)/1000
+
+    times = []
+    for k in range(len(split_lc)):
+        if len(split_lc[k].time[idx[0]:idx[1]])<3 : continue
+        ax0.step(split_lc[k].time[idx[0]:idx[1]]/1000-tmin,
+        split_lc[k].countrate[idx[0]:idx[1]], 
+        where='mid', lw=1, c='k', zorder=2)
+        times.append([split_lc[k].time[idx[0]], split_lc[k].time[idx[1]]])
+
+
+    # Snap intervals for time selection
+    snap_times = np.unique(np.c_[np.asarray(times)/1000-tmin])
+    if len(snap_times)<3: snap_times=None
+
+    # Initiate parameter list 
+    parameters = np.zeros(4)
+
+
+    ## Initiate data containers
+    ## ------------------------------------
+    
+    line1, = ax1.step([],[], c='k', where='mid', lw=1.5, zorder=2, alpha=0.35)
+    line2, = ax2.plot([],[], c='k', lw=1.5, zorder=2, alpha=0.35)
+
+    scat1 = ax1.scatter([],[], c=[], cmap='viridis', marker='s', s=13, zorder=3)
+    scat2 = ax2.scatter([],[], c=[], cmap='viridis', marker='s', s=13, zorder=3)
+
+
+    ## Widgets
+    ## ------------------------
+    
+    timespan = SpanSelector(ax0, read_span_selector, direction="horizontal",
+            useblit=True, props=dict(alpha=0.3, facecolor="coral"), 
+            handle_props=dict(lw=1.25, ls='--'), interactive=True, 
+            drag_from_anywhere=True, ignore_event_outside=True,
+	        grab_range=10, snap_values=snap_times )
+
+
+    ax_ecut = fig.add_subplot(gs[1, 3])
+    ax_tbin = fig.add_subplot(gs[1, 0])
+
+    sld_ecut = Slider( ax_ecut, "", valmin=0, valmax=nbins-1, valinit=nbins//2, 
+    valstep=1, valfmt='%.1f', track_color='silver', color="navy", initcolor=None, 
+    orientation='vertical', handle_style={'size':7, '':'d', 'edgecolor':'k'})
+        
+    sld_tbin = Slider( ax_tbin, "", valmin=0, valmax=nbins-1, valinit=nbins//2,
+    valstep=1, valfmt='%.1f', track_color='silver', color="navy", initcolor=None, 
+    orientation='vertical', handle_style={'size':7, '':'d', 'edgecolor':'k'})
+
+    tbin_lab = ax_tbin.annotate(f'Bin time : ${timebins[nbins//2]:.1f}$ s',
+     xy=(-1,0.5), xycoords='axes fraction', rotation=90, fontsize='small', va='center')
+    ecut_lab = ax_ecut.annotate(f'Cutoff energy : ${energies[nbins//2]:.1f}$ keV', 
+    xy=(1,0.5), xycoords='axes fraction', rotation=-90, fontsize='small', va='center')
+
+    sld_ecut.on_changed(read_sliders)
+    sld_tbin.on_changed(read_sliders)
+    sld_ecut.valtext.set_visible(False)
+    sld_tbin.valtext.set_visible(False)
+    
+    ## Reset button
+    ## ------------- 
+    ax_reset = fig.add_axes([0, 0, 0.06, 0.06])
+    clear_button = Button(ax_reset, 'Clear', color='silver', hovercolor='skyblue')
+    clear_button.on_clicked(clear_axes)    
+
+
+    plt.show()
+
+
